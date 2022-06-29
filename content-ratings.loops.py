33965fc94dc7d4634501e1f795fb112b9c3b94f6
Daniel Afanador <danielthemisfits@gmail.com>
2022-06-28 23:05:38 -0300

    Versión 0.2 funcional

--- content-ratings.loops.py
+++ content-ratings.loops.py
@@ -1,4 +1,4 @@
-ratings = ['4+', '4+', '4+', '9+', '12+', '12+', '17+', '17+']
+ratings = ['4+', '4+', '4+', '9+', '12+', '12+', '17+', '17+', '13+']
 
 content_ratings = {}
 
@@ -6,18 +6,13 @@ content_ratings ['4+'] = 0
 content_ratings ['9+'] = 0
 content_ratings ['12+'] = 0
 content_ratings ['17+'] = 0
+content_ratings ['13+'] = 0
 
 for element in ratings:
 
-    if element == '4+': 
-        content_ratings['4+'] += 1
-    if element == '9+': 
-        content_ratings['9+'] += 1
-    if element == '12+': 
-        content_ratings['12+'] += 1
-    if element == '17+': 
-        content_ratings['17+'] += 1
-    #print (content_ratings)
-
-#print (' ')
+    if element in content_ratings: 
+        content_ratings[element] += 1
+    else:
+        content_ratings[element] = 1
+    
 print (content_ratings)
