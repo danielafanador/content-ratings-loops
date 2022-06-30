2f3d077f24bd56ee8f7b564a2322d034bdaccd64
Daniel Afanador <danielthemisfits@gmail.com>
2022-06-30 17:00:54 -0300

    Versión con todos los cambios sugeridos por Eric

--- content-ratings.loops.py
+++ content-ratings.loops.py
@@ -1,12 +1,6 @@
-ratings = ['4+', '4+', '4+', '9+', '12+', '12+', '17+', '17+', '13+']
+ratings = ['4+', '4+', '4+', '9+', '12+', '12+', '17+', '17+']
 
 content_ratings = {}
-
-content_ratings ['4+'] = 0
-content_ratings ['9+'] = 0
-content_ratings ['12+'] = 0
-content_ratings ['17+'] = 0
-content_ratings ['13+'] = 0
 
 for element in ratings:
 
@@ -14,5 +8,5 @@ for element in ratings:
         content_ratings[element] += 1
     else:
         content_ratings[element] = 1
-    
+
 print (content_ratings)
